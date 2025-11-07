2a095f90e2873ed0c263de01ecc16033908a78ba
minhyeokCho <whalsgur0000@naver.com>
2025-11-07 15:08:45 +0900

    weww

---
+++ sub/brand.php
@@ -0,0 +1,40 @@
+<?php include dirname(__DIR__) . '/include/top.php'; ?>
+<?php include dirname(__DIR__) . '/include/header.php'; ?>
+	<div id="container" class="sub_container overview_container">
+		<!-- sub visual -->
+		<section class="sec_sub_visual">
+			<div class="sub_inner">
+				<div class="sub_visual">
+					<figure class="img">
+						<picture>
+								<source srcset="/teaser2/assets/images/sub/sub_visual_overview.png" media="(min-width: 481px)">
+								<source srcset="/teaser2/assets/images/sub/sub_visual_overview_mb.png" media="(max-width: 480px)">
+								<img src="/teaser2/assets/images/sub/sub_visual_overview.png" alt="">
+							</picture>
+					</figure>	
+					<h2 data-aos="fade-up" data-aos-duration="1000">brand</h2>
+					<p data-aos="fade-up" data-aos-duration="1200">브랜드소개</p>
+				</div>
+			</div>
+		</section>
+		<!-- // sub visual -->
+		<!-- sub content -->
+		<section class="sub_content">
+			<div class="sb_inner">
+				<picture class="img">
+					<source srcset="/teaser2/assets/images/sub/brand_pc.png" media="(min-width: 481px)">
+					<source srcset="/teaser2/assets/images/sub/brand_mb.png" media="(max-width: 480px)">
+					<img src="/teaser2/assets/images/sub/brand_pc.png" alt="">
+				</picture>
+			</div>
+			<div class="obj_wrap">
+				<picture>
+					<source srcset="/teaser2/assets/images/sub/sub_text_obj.png" media="(min-width: 481px)">
+					<source srcset="/teaser2/assets/images/sub/sub_text_obj_mb.png" media="(max-width: 480px)">
+					<img src="/teaser2/assets/images/sub/sub_text_obj.png" alt="">
+				</picture>
+			</div>
+		</section>
+		<!-- // sub content -->
+	</div>
+	<?php include dirname(__DIR__) . '/include/footer.php'; ?>
---
+++ sub/location.php
@@ -0,0 +1,40 @@
+<?php include dirname(__DIR__) . '/include/top.php'; ?>
+<?php include dirname(__DIR__) . '/include/header.php'; ?>
+	<div id="container" class="sub_container overview_container">
+		<!-- sub visual -->
+		<section class="sec_sub_visual">
+			<div class="sub_inner">
+				<div class="sub_visual">
+					<figure class="img">
+						<picture>
+								<source srcset="/teaser2/assets/images/sub/sub_visual_overview.png" media="(min-width: 481px)">
+								<source srcset="/teaser2/assets/images/sub/sub_visual_overview_mb.png" media="(max-width: 480px)">
+								<img src="/teaser2/assets/images/sub/sub_visual_overview.png" alt="">
+							</picture>
+					</figure>	
+					<h2 data-aos="fade-up" data-aos-duration="1000">location</h2>
+					<p data-aos="fade-up" data-aos-duration="1200">입지환경</p>
+				</div>
+			</div>
+		</section>
+		<!-- // sub visual -->
+		<!-- sub content -->
+		<section class="sub_content">
+			<div class="sb_inner">
+				<picture class="img">
+					<source srcset="/teaser2/assets/images/sub/location_pc.png" media="(min-width: 481px)">
+					<source srcset="/teaser2/assets/images/sub/location_mb.png" media="(max-width: 480px)">
+					<img src="/teaser2/assets/images/sub/location_pc.png" alt="">
+				</picture>
+			</div>
+			<div class="obj_wrap">
+				<picture>
+					<source srcset="/teaser2/assets/images/sub/sub_text_obj.png" media="(min-width: 481px)">
+					<source srcset="/teaser2/assets/images/sub/sub_text_obj_mb.png" media="(max-width: 480px)">
+					<img src="/teaser2/assets/images/sub/sub_text_obj.png" alt="">
+				</picture>
+			</div>
+		</section>
+		<!-- // sub content -->
+	</div>
+	<?php include dirname(__DIR__) . '/include/footer.php'; ?>
